1b7032774231061b99dca7481f3ea09eec647e4a
kazugyh-netizen <kazugyh@gmail.com>
2025-10-27 13:07:16 +0100

    first commit

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'currency-calc-v1';
+const urlsToCache = [
+  '/',
+  '/為替レート.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
